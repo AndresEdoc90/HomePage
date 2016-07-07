cc5b3336ed3336a8a9142dac7010d73d239b0272
Giovanny hernandez <ghernandez1990@outlook.com>
2016-07-07 16:49:33 -0400

    Working on puttin button to cover bio

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 $(".img-circle").hide().show("slow");
+// hide bio
+$("#bio").hide();
+// add button
+$("bio").append("<button type='button' class='btn btn-primary'>Primary</button>");
+// when button presses
+$("button").click(function() {
+	$(this).prev().show();
+
+	$(this).remove();
+});
