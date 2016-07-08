3c76eedf36876dd7601e7f74daac0c20b2270b62
Giovanny Hernandez <ghernandez1990@outlook.com>
2016-07-08 14:00:03 -0400

    Initial setup of button to show hidden bio and change background color

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $(".img-circle").hide().show("slow");
 // hide bio
-$("#bio").hide();
+
+$(".bio strong").hide();
 // add button
-$("bio").append("<button type='button' class='btn btn-primary'>Primary</button>");
+$(".bio").append("<button>Bio</button>");
 // when button presses
 $("button").click(function() {
 	$(this).prev().show();
+	$("body").css("background-color", "white");
 
 	$(this).remove();
 });
